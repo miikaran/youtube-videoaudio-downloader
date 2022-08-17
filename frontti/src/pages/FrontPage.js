4e9cf81ceb85881ea64a6286fb7660ac81a2c65d
miikaran <42628@edu.raseko.fi>
2022-08-17 21:23:26 +0300

    inprogress loadmore button

--- frontti/src/pages/FrontPage.js
+++ frontti/src/pages/FrontPage.js
@@ -9,6 +9,7 @@ export default function FrontPage(){
     const [hasSearched, sethasSearched] = useState(false)
     const [search, setSearch] = useState('')
     const [errors, setErrors] = useState('')
+    let resultAmount = 10;
     const port = 4000
     const youtubeAPIKey = ""
 
@@ -16,15 +17,29 @@ export default function FrontPage(){
     const url = `http://localhost:${port}/VIDEOS/`
 
     //YOUTUBE DATA API
-    const youtubeURL = `https://www.googleapis.com/youtube/v3/search?key=${youtubeAPIKey}&type=video&part=snippet&maxResults=10&q=${userSearch}`
+    const youtubeURL = `https://www.googleapis.com/youtube/v3/search?key=${youtubeAPIKey}&type=video&part=snippet&maxResults=${resultAmount}&q=${userSearch}`
+
+
+
 
     const fetchYoutubeData = async () => {
 
         const response = await fetch(youtubeURL)
         const data = await response.json()
         console.log(data)
+
         sethasSearched(true)
         setvideoThumbnail(data.items)
+    }
+
+
+
+    
+    const loadMore = () => {
+
+        resultAmount += 10;
+        fetchYoutubeData();
+        
     }
 
 
@@ -70,9 +85,9 @@ export default function FrontPage(){
 
     return(
         <div>
-            <div class="mt-72 p-14 max-w-4xl mx-auto bg-gray-300 rounded shadow-lg flex items-center">
+            <div class="mt-20 p-14 max-w-7xl mx-auto bg-gray-300 rounded shadow-lg flex items-center">
                 <div class="mx-5">
-                    <div class="text-4xl font-bold text-center text-black">ENTER THE VIDEO NAME HERE:</div>
+                    <div class="text-4xl font-bold text-center text-black">DOWNLOAD VIDEOS BY NAME OR URL:</div>
                         <div class="max-w-xl mx-auto mt-8">
                             <form action="#" class="sm:gap-4 sm:flex">
                     
@@ -107,35 +122,42 @@ export default function FrontPage(){
                     </div>
                     {hasSearched ? (
                      <div class="mt-10 mx-2">
-                        <div class="font-bold text-2xl">
-                        RESULTS:
+                        <div class="font-bold text-2xl flex justify-center">
+                            ALL THE RESULTS
                         </div>
+
+                        <hr class="mt-5"></hr>
 
                         {videoThumbnail.map(info => {
                             
                             return (
-                                <a href="https://www.youtube.com/watch?v=">
-                                    <div class="bg-gray-200">
-
-                                        <div class="mt-10 bg-gray-200 p-5 flex">
-                                            <div class="bg-gray-500 p-2">
-                                                <img src={info.snippet.thumbnails.default.url}></img>
-                                            </div>
-                                            <div class="">
-                                                <p class="mt-2 font-bold text-lg mx-4"><span class="font-medium text-md">{info.snippet.title}</span></p>
-                                                <p class="text-xs mx-4 py-2">{info.snippet.description}</p>
-                                            </div>
+                                
+                                <a href={'https://www.youtube.com/watch?v=' + info.id.videoId}>
+                                    <div class="mt-10 bg-gray-200 p-5 flex">
+                                        <div class="p-3">
+                                            <img src={info.snippet.thumbnails.medium.url} width = "250"></img>
                                         </div>
-                                        
-
-                                    </div>
+                                        <div class="">
+                                            <p class="mx-4 text-2xl font-bold">{info.snippet.channelTitle}</p>
+                                            <p class="mt-1 font-bold text-lg mx-4"><span class="font-medium text-md">{info.snippet.title}</span></p>
+                                            <p class="text-xs mx-4 py-2">{info.snippet.description}</p>
+                                        </div>
+                                    </div>    
                                 </a>
+                                
                             )
 
                         })}
+
+                    <div class="mt-10">
+                        <button onClick={loadMore} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            LOAD MORE...
+                        </button>
+                    </div>
                         
                     </div>
                     ): null}
+
                 </div>
             </div>
         </div>
