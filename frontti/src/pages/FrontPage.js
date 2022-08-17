4baa80718114bc4520d52225c2b775adf97d4314
miikaran <42628@edu.raseko.fi>
2022-08-17 20:38:03 +0300

    setup youtube data API + display JSON results by keyword

--- frontti/src/pages/FrontPage.js
+++ frontti/src/pages/FrontPage.js
@@ -5,22 +5,37 @@ import { useState } from 'react'
 export default function FrontPage(){
 
     const [userSearch, setuserSearch] = useState('')
-    const [videoThumbnail, setvideoThumbnail] = useState('')
+    const [videoThumbnail, setvideoThumbnail] = useState([])
+    const [hasSearched, sethasSearched] = useState(false)
     const [search, setSearch] = useState('')
     const [errors, setErrors] = useState('')
     const port = 4000
+    const youtubeAPIKey = ""
 
-
-    //API ENDPOINT
+    //SERVERSIDE API ENDPOINT
     const url = `http://localhost:${port}/VIDEOS/`
 
-    const fetchData = async() => {
+    //YOUTUBE DATA API
+    const youtubeURL = `https://www.googleapis.com/youtube/v3/search?key=${youtubeAPIKey}&type=video&part=snippet&maxResults=10&q=${userSearch}`
+
+    const fetchYoutubeData = async () => {
+
+        const response = await fetch(youtubeURL)
+        const data = await response.json()
+        console.log(data)
+        sethasSearched(true)
+        setvideoThumbnail(data.items)
+    }
+
+
+
+    /*const fetchServerData = async() => {
 
         const response = await fetch(url)
         const data = await response.json()
         console.log(data.result.info)
         setvideoThumbnail(data.result.info)
-    }
+    }*/
 
 
     const sendDataToAPI = async () => {
@@ -46,7 +61,7 @@ export default function FrontPage(){
             console.log(err)
         }
 
-        fetchData();
+        /*fetchData();*/
     }
 
 
@@ -55,9 +70,9 @@ export default function FrontPage(){
 
     return(
         <div>
-            <div class="mt-72 p-14 max-w-2xl mx-auto bg-gray-300 rounded shadow-lg flex items-center">
+            <div class="mt-72 p-14 max-w-4xl mx-auto bg-gray-300 rounded shadow-lg flex items-center">
                 <div class="mx-5">
-                    <div class="text-4xl font-bold text-center text-black">ENTER THE VIDEO URL HERE:</div>
+                    <div class="text-4xl font-bold text-center text-black">ENTER THE VIDEO NAME HERE:</div>
                         <div class="max-w-xl mx-auto mt-8">
                             <form action="#" class="sm:gap-4 sm:flex">
                     
@@ -82,7 +97,7 @@ export default function FrontPage(){
                             </button>
 
                             <button
-                            
+                            onClick={fetchYoutubeData}
                             type="submit"
                             class="flex items-center justify-center w-full px-5 py-3 mt-4 text-white transition rounded-md bg-rose-600 sm:mt-0 sm:w-auto group focus:outline-none focus:ring focus:ring-yellow-400"
                             >
@@ -90,6 +105,37 @@ export default function FrontPage(){
                         </button>
                         </form>
                     </div>
+                    {hasSearched ? (
+                     <div class="mt-10 mx-2">
+                        <div class="font-bold text-2xl">
+                        RESULTS:
+                        </div>
+
+                        {videoThumbnail.map(info => {
+                            
+                            return (
+                                <a href="https://www.youtube.com/watch?v=">
+                                    <div class="bg-gray-200">
+
+                                        <div class="mt-10 bg-gray-200 p-5 flex">
+                                            <div class="bg-gray-500 p-2">
+                                                <img src={info.snippet.thumbnails.default.url}></img>
+                                            </div>
+                                            <div class="">
+                                                <p class="mt-2 font-bold text-lg mx-4"><span class="font-medium text-md">{info.snippet.title}</span></p>
+                                                <p class="text-xs mx-4 py-2">{info.snippet.description}</p>
+                                            </div>
+                                        </div>
+                                        
+
+                                    </div>
+                                </a>
+                            )
+
+                        })}
+                        
+                    </div>
+                    ): null}
                 </div>
             </div>
         </div>
