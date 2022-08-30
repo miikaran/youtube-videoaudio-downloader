acb54ef7a597a0552e0c427d762e357b5cc09957
miikaran <42628@edu.raseko.fi>
2022-08-30 19:04:08 +0300

    small fixes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const setupBodyparser = require('./setup/bodyparser')
 const setupRouting = require('./setup/routing')
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 5000;
 
 
 const app = express();
